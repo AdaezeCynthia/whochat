c585d9030b10a86a6a39405326858f575336270e
AdaezeCynthia <nackycynthia@gmail.com>
2023-10-07 11:58:00 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Login from './pages/Login';
+import Navbar from './Components/Navbar';
+import './App.css';
+
+function App() {
+ 
+
+  return (
+    
+      <div >
+          <Navbar/>
+        <Login/>
+      
+      </div>
+     
+   
+  )
+}
+
+export default App
---
+++ src/pages/Login.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Hello there👋 </h1>
+          <p className="py-6">
+          social media mavericks! 📱✨ Get ready to dive into a world of endless memes, epic stories, 
+          and all things share-worthy. Let's make your feed the envy of the digital universe! 🚀
+          </p>
+          <button className="btn btn-primary">Login</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
