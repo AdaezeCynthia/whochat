4f0deef2312e4736d3c4f4661d75623f98905621
AdaezeCynthia <nackycynthia@gmail.com>
2023-10-08 11:47:22 -0700

    second 8th oct

--- src/Components/Sendmessage.jsx
+++ src/Components/Sendmessage.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import { useState } from "react";
 
 const Sendmessage = () => {
-  return (
-    <div className='bg-gray-200 fixed bottom-0 w-full 
-    py-10 shadow-lg'>Sendmessage</div>
-  )
-}
+  const [value, setValue] = useState("");
 
-export default Sendmessage
+  const handleSm = (e) => {
+    e.preventDefault();
+    console.log(value);
+  };
+
+  return (
+    <div
+      className="bg-gray-200 fixed bottom-0 w-full 
+    py-10 shadow-lg"
+    >
+      <form onSubmit={handleSm} className="containerWrap flex">
+        <input
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          className="input w-full focus:outline-none bg-gray-100
+            rounded-r-none"
+          type="text"
+        />
+        <button
+          type="submit"
+          className="w-auto bg-gray-500 text-white 
+            rounded-r-lg px-5 text-sm"
+        >
+          Send
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Sendmessage;
