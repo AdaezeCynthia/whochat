56d349cf14f2c93c58eecbc124caf62865e88c22
AdaezeCynthia <nackycynthia@gmail.com>
2024-02-02 10:26:01 -0800

    login fixed

--- src/Routes/PrivateRoutes.jsx
+++ src/Routes/PrivateRoutes.jsx
@@ -1,9 +1,10 @@
 import { Navigate } from "react-router-dom";
+import { UserAuth } from "../Context/AuthContext";
 
 
 export const PrivateRoutes = ({children}) => {
 
-  const currentUser = true;
+  const{currentUser, } = UserAuth ();
   if(!currentUser){
     return<Navigate  to= '/' replace={true} />
   }
