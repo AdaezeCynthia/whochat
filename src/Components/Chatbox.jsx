0ff7efa0698f61b6600a93bd0e743dc9b5917b93
AdaezeCynthia <nackycynthia@gmail.com>
2024-02-09 11:45:25 -0800

    firestore fixed

--- src/Components/Chatbox.jsx
+++ src/Components/Chatbox.jsx
@@ -1,40 +1,42 @@
-import React, { useEffect } from 'react';
-import Message from './Message';
+import React, { useEffect } from "react";
+import Message from "./Message";
 import { collection, query, where, onSnapshot } from "firebase/firestore";
+import { db } from "../Firebase";
 
 const Chatbox = () => {
-
-  const messages =[
+  const messages = [
     {
-      id:1,
-      text:"Hello there",
-      name: 'Ada'
+      id: 1,
+      text: "Hello there",
+      name: "Ada",
     },
     {
-      id:2,
-      text:"Hi",
-      name: 'Eze'
-    }
-  ]
+      id: 2,
+      text: "Hi",
+      name: "Eze",
+    },
+  ];
 
   useEffect(() => {
-    const q = query(collection(db, "messages"),);
+    const q = query(
+
+      collection(db, "messages")
+      );
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       const messages = [];
       querySnapshot.forEach((doc) => {
-         // messages.push(doc.data().name);
-         console.log (doc.data())
+        // messages.push(doc.data().name);
+        console.log(doc.data());
       });
     });
-    
-  },[]);
+  }, []);
   return (
-    <div className='pb-44 pt-20 containerWrap '>
-          {messages.map(message => (
-            <Message  key={message.id}     message ={message}/>
-          ))}
+    <div className="pb-44 pt-20 containerWrap ">
+      {messages.map((message) => (
+        <Message key={message.id} message={message} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Chatbox
+export default Chatbox;
