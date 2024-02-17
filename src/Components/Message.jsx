661c4476c7b88d102d6a2360cc464f6de9dfc2f1
AdaezeCynthia <nackycynthia@gmail.com>
2024-02-16 23:09:44 -0800

    login pro

--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -1,17 +1,19 @@
 import React from "react";
+import { UserAuth } from "../Context/AuthContext";
 
 const Message = ({ message }) => {
+  const {currentUser } = UserAuth();
   console.log(message)
   return (
     <div>
-      <div className="chat chat-start">
+      <div className={`chat ${message.uid === currentUser.uid ? "chat-end" : "chat-start"}`}>
         <div className="chat-image avatar">
           <div className="w-10 rounded-full">
-            <img src='' />
+            <img src={message.avatar} />
           </div>
         </div>
         <div className="chat-header">
-          {message.displayName
+          {message.name
           }
         
         </div>
